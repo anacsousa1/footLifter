83d5362d5642b20ecae11c62d1c37863270f2e72
Ana <anacsousa1@gmail.com>
2015-07-01 17:27:40 -0300

    my_plotter: do the plot in real time and save the result at the end.

--- footLifter_simple.py
+++ footLifter_simple.py
@@ -1,11 +1,11 @@
 # ######################################################################################################################
-# ## stimulation_test.py
-# ## Description: testing the stimulator
-# ## Library needed: stimulator, serial, sys libraries
+# ## footLifter.py
+# ## Description: do the foot Lifter
+# ## Library needed: stimulator, serial, imu, sys libraries
 # ## Python interpreter: Anaconda 2.2.0 (python 2.7)
 # ## Author: Ana Carolina Cardoso de Sousa
 # ## Email: anacsousa1@gmail.com
-# ## Created: Jun 04th 2015
+# ## Created: Jul 1st 2015
 # ######################################################################################################################
 
 __authors__ = [
@@ -14,15 +14,22 @@ __authors__ = [
 
 
 # Importing...
+import subject_profile
 import serial
 import sys
 import stimulator
 
-# Greetings
-print "Welcome to ours stimulator tester, let\'s get this started?\n"
 
-# Ports and addresses
+# Important variables
+user_name = 'antonio'
 serialPortStimulator = 'COM6'        # in windows, verify "Manage Devices"
+freq = 50
+channels = 1
+
+# #################################### Greetings
+print "Welcome to ours Foot Lifter, let\'s get this started?\n"
+
+# #################################### Ports and addresses
 
 # Open ports
 print '\tWe are trying to connect to the Stimulator to port ' + serialPortStimulator + '.'
@@ -40,31 +47,30 @@ if not serialPortStimulator.isOpen():  # verify if it is already open
 stim = stimulator.Stimulator(serialPortStimulator)     # Construct object
 print '\t\tWe are connected! Now, we are going to initialize the stimulator.!\n'
 
+# #################################### Subject Profile
+try:
+    [current, pulse_width] = subject_profile.user[user_name]()
+except:
+        [current, pulse_width] = subject_profile.case_default()
 
-# Initialize stimulator
-freq = 50
-current = [8]
-pulse_width = [500]
-channels = 1
+print str(current)
+
+
+# #################################### Initialize stimulator
 print "\tStandard parameters: " + str(freq) + "Hz " + str(current) + "mA " + str(pulse_width) + "us."
-
 stim.initialization(freq, channels)
 
 print "\n\tInitialized! Stimulating channel."
+stim.update(channels, pulse_width, current)  # First electrical stimulator signal update
 
-# First electrical stimulator signal update
-stim.update(channels, pulse_width, current)
-
-# Loop to change the pulse_width
+# #################################### Loop to change the pulse_width
 
 exit_key = 'esc'
-msg = "\n\tWrite a different width pulse (range between 100 and 500) like this [p]:\n\t(Write " + exit_key + " to exit)\n\t\t>>"
+msg = "\n\tStimulation started\n\t\t>>"
 
 key_input = raw_input(msg)      # reads the keyboard
 
 while key_input != exit_key:
-    pulse_width[0] = int(key_input)                 # change pulse_width variable
-    stim.update(channels, pulse_width, current)     # update the stimulator signal
     key_input = raw_input(msg)                      # reads the keyboard
 
 # Stop the stimulator
---
+++ src/subject_profile.py
@@ -0,0 +1,48 @@
+# ######################################################################################################################
+# ## subject_profile.py
+# ## Description: define user profile
+# ## Library needed:           libraries
+# ## Python interpreter: Anaconda 2.2.0 (python 2.7)
+# ## Author: Ana Carolina Cardoso de Sousa
+# ## Email: anacsousa1@gmail.com
+# ## Created: Jul 1st 2015
+# ######################################################################################################################
+
+
+__authors__ = [
+    "\"Ana de Sousa\" <anacsousa1@gmail.com>",
+]
+
+def ana_claudia():
+    print "User: Ana Claudia"
+    current = [8]
+    pulse_width = [100]
+    return current, pulse_width
+def ana_carolina():
+    print "User: Ana Carolina"
+    current = [8]
+    pulse_width = [200]
+    return current, pulse_width
+def cristina():
+    print "User: Cristina"
+    current = [8]
+    pulse_width = [300]
+    return current, pulse_width
+def claudir():
+    print "User: Claudir"
+    current = [8]
+    pulse_width = [400]
+    return current, pulse_width
+def antonio():
+    print "User: Antonio"
+    current = [8]
+    pulse_width = [500]
+    return current, pulse_width
+def case_default():
+    print "default"
+
+user = {'ana_claudia': ana_claudia, 'ana_carolina': ana_carolina, 'cristina': cristina, 'claudir': claudir, 'antonio': antonio}
+
+if __name__ == '__main__':
+    print user()
+
